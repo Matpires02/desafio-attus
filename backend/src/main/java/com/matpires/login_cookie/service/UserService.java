eee5362102e0e7a50a45cde941aa8ed45f3593fe
Matheus Pires <61023277+Matpires02@users.noreply.github.com>
2026-05-27 20:18:24 -0300

    Update UserService.java

--- backend/src/main/java/com/matpires/login_cookie/service/UserService.java
+++ backend/src/main/java/com/matpires/login_cookie/service/UserService.java
@@ -1,9 +1,14 @@
 package com.matpires.login_cookie.service;
 
 import com.matpires.login_cookie.dto.RegisterRequestDto;
+import com.matpires.login_cookie.dto.UpdateUserDto;
+import com.matpires.login_cookie.dto.UserDto;
 import com.matpires.login_cookie.entity.Role;
 import com.matpires.login_cookie.entity.User;
 import com.matpires.login_cookie.enums.RoleName;
+import com.matpires.login_cookie.exceptions.EmailAlreadyUsedException;
+import com.matpires.login_cookie.exceptions.UserNotFoundException;
+import com.matpires.login_cookie.mapper.UserMapper;
 import com.matpires.login_cookie.repository.RoleRepository;
 import com.matpires.login_cookie.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,6 +27,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
 
     public void register(RegisterRequestDto dto) {
         log.debug("Registering user {}", dto.getEmail());
@@ -52,5 +59,56 @@ public class UserService {
         user.setActivated(true);
 
         userRepository.save(user);
+    }
+
+    public UserDto update(UpdateUserDto dto) {
+        log.debug("Updating user {}", dto.getEmail());
+        User user = userRepository.findById(dto.getId()).orElseThrow(UserNotFoundException::new);
+
+        if (dto.getRoles() != null && !dto.getRoles().isEmpty()) {
+            var roles = dto.getRoles().stream()
+                    .map(roleStr -> roleRepository.findByName(RoleName.valueOf(roleStr))
+                            .orElseThrow(() -> new RuntimeException("Role não encontrada: " + roleStr)))
+                    .collect(Collectors.toSet());
+            roles.add(roleRepository.findByName(RoleName.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Role USER não encontrada")));
+
+            user.setRoles(roles);
+        }
+
+        if (dto.getPassword() != null) {
+            user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        }
+
+        if (dto.getActivated() != null) {
+            user.setActivated(dto.getActivated());
+        }
+
+        if (dto.getEmail() != null) {
+            if (userRepository.existsByEmailAndIdIsNot(dto.getEmail(), user.getId())) {
+                throw new EmailAlreadyUsedException();
+            }
+            user.setEmail(dto.getEmail());
+        }
+
+        User saved = userRepository.save(user);
+
+        log.debug("User {} updated", saved.getEmail());
+        return userMapper.toDTO(saved);
+
+    }
+
+    public UserDto findById(UUID id) {
+        log.debug("Finding user {}", id);
+        return userRepository.findById(id).map(userMapper::toDTO).orElseThrow(UserNotFoundException::new);
+    }
+
+    public void deleteUser(UUID id) {
+        log.debug("Deleting user {}", id);
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return;
+        }
+        throw new UserNotFoundException();
     }
 }
