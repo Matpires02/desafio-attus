667c805b71f9635b80ad917d23c6d0b07df1c7b4
Matheus Pires <61023277+Matpires02@users.noreply.github.com>
2026-05-27 00:12:35 -0300

    Update auth-guard.ts

--- frontend/src/app/guards/auth-guard.ts
+++ frontend/src/app/guards/auth-guard.ts
@@ -1,12 +1,10 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
 import {UserStore} from '../store/user.store';
-import {LoginService} from '../service/login/login.service';
 
-export const authGuard: CanActivateFn = async (route, state) => {
+export const authGuard: CanActivateFn = (route, state) => {
   const userStore = inject(UserStore);
   const router = inject(Router);
-  const loginService = inject(LoginService);
   console.log(route.data)
   const role = route.data['roles'];
   if (!role) {
