500506ba660e759223278c61cb9604477708e02c
Matheus Pires <61023277+Matpires02@users.noreply.github.com>
2026-05-26 21:01:20 -0300

    Update login-service.ts

--- frontend/src/app/service/login/login-service.ts
+++ frontend/src/app/service/login/login-service.ts
@@ -2,24 +2,33 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {LoginModel} from '../../models/login/login.model';
+import {tap} from 'rxjs';
+import {AuthService} from '../auth/auth.service';
+import {UserModel} from '../../models/user/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
   private readonly baseUrl = `${environment.apiUrl}/auth`;
-  constructor(private client: HttpClient) {
+  constructor(private client: HttpClient, private auth: AuthService) {
   }
 
   login(loginModel: LoginModel) {
-    return this.client.post(`${this.baseUrl}/login`, loginModel);
+    return this.client.post<any>(`${this.baseUrl}/login`, loginModel, {observe: 'response'}).pipe(tap(res => {
+      this.getUser().subscribe(user => this.auth.setUser(user))
+    }));
   }
 
   logout() {
-    return this.client.post(`${this.baseUrl}/logout`, {});
+    return this.client.post(`${this.baseUrl}/logout`, {}, { withCredentials: true}).pipe(tap(()=> this.auth.clear()));
   }
 
   refreshToken() {
-    return this.client.post(`${this.baseUrl}/refresh`, {});
+    return this.client.post(`${this.baseUrl}/refresh`, {}, { withCredentials: true});
+  }
+
+  getUser() {
+    return this.client.get<UserModel>(`${this.baseUrl}/user`, { withCredentials: true});
   }
 }
