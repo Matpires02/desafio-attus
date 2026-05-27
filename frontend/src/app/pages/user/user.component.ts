9e2ad788c69d016499fb65e4d7c402b58a06c531
Matheus Pires <61023277+Matpires02@users.noreply.github.com>
2026-05-27 20:21:58 -0300

    Add user form and update handler to user page
    Replace placeholder with <app-user-form> in the user page and wire it up to the store and UI. The component now imports UserFormComponent, injects UserStore and MatSnackBar, exposes UserRole for template checks, and handles actionComplete with onUpdateUser() which shows a snackbar confirmation. This enables editing the current user and gives feedback when updates are completed.

--- frontend/src/app/pages/user/user.component.ts
+++ frontend/src/app/pages/user/user.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {UserFormComponent} from "../../components/user-form/user-form.component";
+import {UserStore} from '../../store/user.store';
+import {UserRole} from '../../models/user/user-role.enum';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [
+    UserFormComponent
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-export class UserComponent {}
+export class UserComponent {
+  protected userStore = inject(UserStore);
+  protected readonly UserRole = UserRole;
+  private snackBar = inject(MatSnackBar);
+
+  protected onUpdateUser() {
+    this.snackBar.open('Update realizado com sucesso!', 'Fechar', {duration: 3000});
+  }
+}
