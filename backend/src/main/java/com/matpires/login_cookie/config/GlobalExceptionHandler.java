3255bb051bc487fee7da2761b1685964175f454e
Matheus Pires <61023277+Matpires02@users.noreply.github.com>
2026-05-28 00:14:22 -0300

    Update GlobalExceptionHandler.java

--- backend/src/main/java/com/matpires/login_cookie/config/GlobalExceptionHandler.java
+++ backend/src/main/java/com/matpires/login_cookie/config/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.matpires.login_cookie.config;
 
+import com.matpires.login_cookie.exceptions.EmailAlreadyUsedException;
 import com.matpires.login_cookie.exceptions.InvalidCredentialsException;
+import com.matpires.login_cookie.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,10 +13,10 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(InvalidCredentialsException.class)
-    public ResponseEntity<?> handleInvalidCredentials(InvalidCredentialsException ex) {
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<?> handleInvalidCredentials(Exception ex) {
         return ResponseEntity
-                .status(HttpStatus.FORBIDDEN)
+                .status(HttpStatus.BAD_REQUEST)
                 .body(Map.of("error", ex.getMessage()));
     }
 }
